94f05d2014e3de1a8de7f657be97aae3aea9ce0f
Andrew Minion <andrew@andrewrminion.com>
2018-03-12 16:22:46 -0400

    initial commit

---
+++ digitalkm.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:     DigitalKM
+ * Plugin URI:      https://digitalkm.org
+ * Description:     Digital KM Functionality
+ * Author:          AndrewRMinion Design
+ * Author URI:      https://andrewrminion.com
+ * Text Domain:     digitalkm
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Digitalkm
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
