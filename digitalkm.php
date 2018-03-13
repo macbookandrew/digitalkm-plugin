1ef3ce8db6ef22365d3362220f64b8ea92b63b29
Andrew Minion <andrew@andrewrminion.com>
2018-03-13 16:46:38 -0400

    add function to format date range

--- digitalkm.php
+++ digitalkm.php
@@ -51,3 +51,31 @@ function dkm_acf_json_load_point( $path ) {
 }
 add_filter( 'acf/settings/load_json', 'dkm_acf_json_load_point' );
 
+/**
+ * Format date range
+ * @param  string $begin_date Ymd-formatted begin date
+ * @param  string $end_date   Ymd-formatted end date
+ * @return string human-formatted date range
+ */
+function format_date_range( $begin_date, $end_date = NULL ) {
+	$begin = new DateTime();
+	$begin->setDate( substr( $begin_date, 0, 4 ), substr( $begin_date, 4, 2 ), substr( $begin_date, 6, 2 ) );
+	$formatted_date = $begin->format( 'F j, Y' );
+
+	// end date
+	if ( ! is_null( $end_date ) && ! empty( $end_date ) ) {
+		$end = new DateTime();
+		$end->setDate( substr( $end_date, 0, 4 ), substr( $end_date, 4, 2 ), substr( $end_date, 6, 2 ) );
+
+		// format is based on distance between dates
+		if ( $begin->format( 'Y' ) !== $end->format( 'Y' ) ) {
+			$formatted_date .= '&ndash;' . $end->format( 'F j, Y' );
+		} elseif ( $begin->format( 'm' ) !== $end->format( 'm' ) ) {
+			$formatted_date = $begin->format( 'F j' ) . '&ndash;' . $end->format( 'F j, Y' );
+		} elseif ( $begin->format( 'd' ) !== $end->format( 'd' ) ) {
+			$formatted_date = $begin->format ( 'F j' ) . '&ndash;' . $end->format( 'j, Y' );
+		}
+	}
+
+	return $formatted_date;
+}
