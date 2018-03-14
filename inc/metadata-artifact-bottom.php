489e6db8cda027566526b7332396c222bdd03358
Andrew Minion <andrew@andrewrminion.com>
2018-03-14 16:14:58 -0400

    move metadata to sidebars

--- inc/metadata-artifact-bottom.php
+++ inc/metadata-artifact-bottom.php
@@ -1,23 +1,28 @@
-<?php
+</main>
 
-$rights_statement = get_field( 'rights_statement' );
-if ( $rights_statement ) {
-	echo '<section id="rights-statement" class="meta">
-	<h2>Rights Statement</h2>
-	<p>';
-	if ( $rights_statement['value'] !== 'other' ) {
-		echo '<a href="' . $rights_statement['value'] . '" rel="nofollow noreferer">' . $rights_statement['label'] . '</a>';
-	} else {
-		the_field( 'rights_statement_other' );
+<aside id="rights" class="sidebar right">
+	<?php
+
+	$rights_statement = get_field( 'rights_statement' );
+	if ( $rights_statement ) {
+		echo '<section id="rights-statement" class="meta">
+		<h2>Rights Statement</h2>
+		<p>';
+		if ( $rights_statement['value'] !== 'other' ) {
+			echo '<a href="' . $rights_statement['value'] . '" rel="nofollow noreferer">' . $rights_statement['label'] . '</a>';
+		} else {
+			the_field( 'rights_statement_other' );
+		}
+		echo '</p>
+		</section>';
 	}
-	echo '</p>
-	</section>';
-}
 
-$grant_funders_statement = get_field( 'grant_funders_statement' );
-if ( $grant_funders_statement ) {
-	echo '<section id="grant-funders-statement" class="meta">
-	<h2>Grant Funders Statement</h2>
-	<p>' . $grant_funders_statement . '</p>
-	</section>';
-}
+	$grant_funders_statement = get_field( 'grant_funders_statement' );
+	if ( $grant_funders_statement ) {
+		echo '<section id="grant-funders-statement" class="meta">
+		<h2>Grant Funders Statement</h2>
+		<p>' . $grant_funders_statement . '</p>
+		</section>';
+	}
+	?>
+</aside>
--- inc/metadata-artifact-top.php
+++ inc/metadata-artifact-top.php
@@ -7,7 +7,7 @@ if ( $images ) {
 	wp_enqueue_style( 'flickity' );
 	wp_enqueue_script( 'flickity' );
 
-	echo '<section id="images" class="meta">
+	echo '<section id="images">
 	<h2>Images</h2>
 	<div class="image-carousel" data-flickity=\'{ "autoPlay": true }\'>';
 	foreach ( $images as $image ) {
@@ -19,93 +19,100 @@ if ( $images ) {
 	echo '</div>
 	</section>';
 }
+?>
 
-$link_to_full_resource = get_field( 'link_to_full_resource' );
-if ( $link_to_full_resource ) {
-	echo '<p><a class="button" id="link-to-full-resource" href="' . $link_to_full_resource . '">View Full Resource</a></p>';
-}
+<aside class="sidebar left">
+	<?php
+	$link_to_full_resource = get_field( 'link_to_full_resource' );
+	if ( $link_to_full_resource ) {
+		echo '<p><a class="button" id="link-to-full-resource" href="' . $link_to_full_resource . '">View Full Resource</a></p>';
+	}
 
-if ( get_field( 'item_begin_date' ) ) {
-	$begin_date = get_field( 'item_begin_date' );
-	$end_date = get_field( 'item_end_date' );
+	if ( get_field( 'item_begin_date' ) ) {
+		$begin_date = get_field( 'item_begin_date' );
+		$end_date = get_field( 'item_end_date' );
 
-	echo '<section id="dates" class="meta">
-	<h2>Date</h2>
-	<p>'. $DKM_Helper->format_date_range( $begin_date, $end_date ) . '</p>
-	<p><a href="' . home_url() . '/timeline/#' . $DKM_Helper->get_timeline_range_query( $begin_date, $end_date ) . '&postid-' . get_the_ID() . '" class="button">See on Timeline</a></p>
-	</section>';
-}
+		echo '<section id="dates" class="meta">
+		<h2>Date</h2>
+		<p>'. $DKM_Helper->format_date_range( $begin_date, $end_date ) . '</p>
+		<p><a href="' . home_url() . '/timeline/#' . $DKM_Helper->get_timeline_range_query( $begin_date, $end_date ) . '&postid-' . get_the_ID() . '" class="button">See on Timeline</a></p>
+		</section>';
+	}
 
-$street_address = get_field( 'street_address' );
-$address_format = get_field( 'address_format' );
-$cities = get_field( 'city' );
-$counties = get_field( 'county' );
-$states = get_field( 'state' );
-$countries = get_field( 'country' );
-$map = get_field( 'map' );
+	$street_address = get_field( 'street_address' );
+	$address_format = get_field( 'address_format' );
+	$cities = get_field( 'city' );
+	$counties = get_field( 'county' );
+	$states = get_field( 'state' );
+	$countries = get_field( 'country' );
+	$map = get_field( 'map' );
 
-if ( $street_address || $cities || $counties || $states || $countries || $coordinates ) {
-	echo '<section id="location" class="meta">
-	<h2>Location</h2>';
-		if ( $street_address ) {
-			echo '<p><strong>Street Address</strong>: ' . $street_address;
-			if ( $address_format['label'] !== 'na' ) {
-				echo ' <span class="small">' . $street_address['value'] . '</span>';
+	if ( $street_address || $cities || $counties || $states || $countries || $coordinates ) {
+		echo '<section id="location" class="meta">
+		<h2>Location</h2>';
+			if ( $street_address ) {
+				echo '<p><strong>Street Address</strong>: ' . $street_address;
+				if ( $address_format['label'] !== 'na' ) {
+					echo ' <span class="small">' . $street_address['value'] . '</span>';
+				}
+				echo '</p>';
 			}
-			echo '</p>';
-		}
 
-		if ( $cities ) {
-			echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_city', 'City' );
-		}
+			if ( $cities ) {
+				echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_city', 'City' );
+			}
 
-		if ( $counties ) {
-			echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_county', 'County' );
-		}
+			if ( $counties ) {
+				echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_county', 'County' );
+			}
 
-		if ( $states ) {
-			echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_state', 'State' );
-		}
+			if ( $states ) {
+				echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_state', 'State' );
+			}
 
-		if ( $countries ) {
-			echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_country', 'Country' );
-		}
+			if ( $countries ) {
+				echo $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_country', 'Country' );
+			}
 
-		if ( $map ) {
-			wp_enqueue_style( 'leaflet' );
-			wp_enqueue_script( 'leaflet' );
-			wp_enqueue_script( 'coordinates-map' );
+			if ( $map ) {
+				wp_enqueue_style( 'leaflet' );
+				wp_enqueue_script( 'leaflet' );
+				wp_enqueue_script( 'coordinates-map' );
 
-			echo '<div id="geoJSONMap' . get_the_ID() . '" class="geo-json-map" data-locationInfo=\'' . $map . '\'></div>';
-		}
+				echo '<div id="geoJSONMap' . get_the_ID() . '" class="geo-json-map" data-locationInfo=\'' . $map . '\'></div>';
+			}
 
-	echo '</section>';
-}
+		echo '</section>';
+	}
 
-$type = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'category', 'Type' );
-if ( $type ) {
-	echo '<section id="type" class="meta">
-		<h2>Type</h2>
-		<p>' . $type . '</p>
-	</section>';
-}
+	$type = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'category', 'Type' );
+	if ( $type ) {
+		echo '<section id="type" class="meta">
+			<h2>Type</h2>
+			<p>' . $type . '</p>
+		</section>';
+	}
 
-$topics = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'post_tag', 'Topics' );
-if ( $type ) {
-	echo '<section id="topics" class="meta">
-		<h2>Topics</h2>
-		<p>' . $topics . '</p>
-	</section>';
-}
+	$topics = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'post_tag', 'Topics' );
+	if ( $type ) {
+		echo '<section id="topics" class="meta">
+			<h2>Topics</h2>
+			<p>' . $topics . '</p>
+		</section>';
+	}
 
-$subject = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_subject', 'Subject' );
-if ( $subject ) {
-	echo '<section id="subject" class="meta">
-		<h2>Subject</h2>
-		<p>' . $subject . '</p>
-	</section>';
-}
+	$subject = $DKM_Helper->get_artifact_tax_html( get_the_ID(), 'artifact_subject', 'Subject' );
+	if ( $subject ) {
+		echo '<section id="subject" class="meta">
+			<h2>Subject</h2>
+			<p>' . $subject . '</p>
+		</section>';
+	}
+	?>
+</aside>
 
-if ( strlen( get_the_content() ) > 0 ) {
-	echo '<h2 id="description">Description</h2>';
-}
+<main class="content">
+	<?php
+	if ( strlen( get_the_content() ) > 0 ) {
+		echo '<h2 id="description">Description</h2>';
+	}
