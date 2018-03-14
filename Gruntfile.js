cb7e4a27344d7e9e6adc9d4bab250f0c019f0b6b
Andrew Minion <andrew@andrewrminion.com>
2018-03-14 11:47:50 -0400

    add build tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,13 +43,59 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
+
+		uglify: {
+			options: {
+				sourceMap: true
+			},
+			custom: {
+				files: {
+					'assets/js/coordinates-map.min.js': ['src/js/coordinates-map.js'],
+				},
+			},
+		},
+
+		watch: {
+			javascript: {
+				files: ['src/js/*.js'],
+				tasks: ['uglify'],
+			},
+			readme: {
+				files: ['readme.txt'],
+				tasks: ['readme'],
+			}
+		},
+
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src : ['assets/**/*.css', '**/*.php', 'assets/**/*.js', '!node_modules'],
+				},
+				options: {
+					watchTask: true,
+					proxy: "https://digitalkm.wordpress.dev",
+					open: "external",
+					host: "andrews-macbook-pro.local",
+					https: {
+						key: "/Users/andrew/github/dotfiles/local-dev.key",
+						cert: "/Users/andrew/github/dotfiles/local-dev.crt",
+					}
+				},
+			},
+		},
 	} );
 
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-browser-sync' );
 	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'default', [ 'i18n','readme' ] );
 	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
 	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.registerTask( 'default', [
+		'browserSync',
+		'watch',
+	]);
 
 	grunt.util.linefeed = '\n';
 
