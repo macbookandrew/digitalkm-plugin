0cc675acd6cd6c60e9cbe9185b7e4a54a9b44b69
Andrew Minion <andrew@andrewrminion.com>
2018-04-04 15:40:07 -0400

    remove groups since many items will be in multiple groups

--- inc/rest-api.php
+++ inc/rest-api.php
@@ -77,11 +77,6 @@ class DKM_Rest extends DKM_Plugin {
 					),
 				);
 
-				$tags = wp_get_post_tags( get_the_ID() );
-				if ( ! empty( $tags ) ) {
-					$this_event['group'] = $tags[0]->name;
-				}
-
 				$begin_date = get_field( 'item_begin_date' );
 				$end_date = get_field( 'item_end_date' );
 
