249d9bfe230920af366d7fba929444fe9a1213f5
Andrew Minion <andrew@andrewrminion.com>
2018-05-15 10:38:22 -0400

    fix mayor dates

--- inc/class-dkm-content.php
+++ inc/class-dkm-content.php
@@ -46,24 +46,24 @@ class DKM_Content extends DKM_Plugin {
 			include 'metadata-artifact-top.php';
 			$content = ob_get_clean() . $content;
 
+			if ( is_singular( 'mayor' ) ) {
+				$birth_date = get_field( 'item_begin_date' );
+				if ( ! empty( $birth_date ) ) {
+					$death_date = get_field( 'item_end_date' );
+
+					$dkm_helper = new DKM_Helper();
+					$lifetime   = '<p class="meta">Born ' . $dkm_helper->format_date_range( $birth_date );
+					if ( ! empty( $death_date ) ) {
+						$lifetime .= '; Died ' . $dkm_helper->format_date_range( $death_date );
+					}
+					$lifetime .= '</p>';
+					$content   = $lifetime . $content;
+				}
+			}
+
 			ob_start();
 			include 'metadata-artifact-bottom.php';
 			$content .= ob_get_clean();
-		}
-
-		if ( is_singular( 'mayor' ) ) {
-			$birth_date = get_field( 'item_begin_date' );
-			if ( ! empty( $birth_date ) ) {
-				$death_date = get_field( 'item_end_date' );
-
-				$dkm_helper = new DKM_Helper();
-				$lifetime   = '<p class="meta">Born ' . $dkm_helper->format_date_range( $birth_date );
-				if ( ! empty( $death_date ) ) {
-					$lifetime .= '; Died ' . $dkm_helper->format_date_range( $death_date );
-				}
-				$lifetime .= '</p>';
-				$content   = $lifetime . $content;
-			}
 		}
 
 		return $content;
